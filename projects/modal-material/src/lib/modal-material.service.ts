67707e37e7aeea189ca967b867446e3617368840
Ivan Antunes <ivanantnes75@gmail.com>
2020-05-29 20:58:29 -0300

    Logica de Modal Finalizado

--- projects/modal-material/src/lib/modal-material.service.ts
+++ projects/modal-material/src/lib/modal-material.service.ts
@@ -151,4 +151,161 @@ constructor(private dialog: MatDialog) { }
     return response.componentInstance.isConfirmed;
   }
 
+  mTError(
+    title: string,
+    description: string,
+    btnCloseTitle: string,
+    width: string,
+    height: string,
+    disableClose: boolean
+  ): void {
+    this.dialog.open(ModalMaterialComponent, {
+      width,
+      height,
+      disableClose,
+      data: {
+        controlType: ModalType.tError,
+        title,
+        description,
+        btnCloseTitle
+      }
+    });
+  }
+
+  mTErrorLog(
+    title: string,
+    description: string,
+    btnCloseTitle: string,
+    btnLogTitle: string,
+    log: string,
+    width: string,
+    height: string,
+    disableClose: boolean
+  ): void {
+    this.dialog.open(ModalMaterialComponent, {
+      width,
+      height,
+      disableClose,
+      data: {
+        controlType: ModalType.tErrorLog,
+        title,
+        description,
+        btnLogTitle,
+        btnCloseTitle,
+        log
+      }
+    });
+  }
+
+  mTErrorConfirm(
+    title: string,
+    description: string,
+    btnCloseTitle: string,
+    btnConfirmTitle: string,
+    width: string,
+    height: string,
+    disableClose: boolean
+  ): Observable<boolean> {
+    const response = this.dialog.open(ModalMaterialComponent, {
+      width,
+      height,
+      disableClose,
+      data: {
+        controlType: ModalType.tErrorConfirm,
+        title,
+        description,
+        btnCloseTitle,
+        btnConfirmTitle,
+      }
+    });
+    return response.componentInstance.isConfirmed;
+  }
+
+  mManualBasic(
+    title: string,
+    description: string,
+    btnCloseTitle: string,
+    icon: string,
+    mBackgroundColorIcon: string,
+    useMatIcon: boolean,
+    width: string,
+    height: string,
+    disableClose: boolean
+  ): void {
+    this.dialog.open(ModalMaterialComponent, {
+      width,
+      height,
+      disableClose,
+      data: {
+        controlType: ModalType.mBasic,
+        title,
+        description,
+        btnCloseTitle,
+        icon,
+        mBackgroundColorIcon,
+        useMatIcon
+      }
+    });
+  }
+
+  mManualLog(
+    title: string,
+    description: string,
+    btnCloseTitle: string,
+    btnLogTitle: string,
+    icon: string,
+    mBackgroundColorIcon: string,
+    useMatIcon: boolean,
+    log: string,
+    width: string,
+    height: string,
+    disableClose: boolean
+  ): void {
+    this.dialog.open(ModalMaterialComponent, {
+      width,
+      height,
+      disableClose,
+      data: {
+        controlType: ModalType.mLog,
+        title,
+        description,
+        btnLogTitle,
+        btnCloseTitle,
+        log,
+        icon,
+        mBackgroundColorIcon,
+        useMatIcon
+      }
+    });
+  }
+
+  mManualConfirm(
+    title: string,
+    description: string,
+    btnCloseTitle: string,
+    btnConfirmTitle: string,
+    icon: string,
+    mBackgroundColorIcon: string,
+    useMatIcon: boolean,
+    width: string,
+    height: string,
+    disableClose: boolean
+  ): Observable<boolean> {
+    const response = this.dialog.open(ModalMaterialComponent, {
+      width,
+      height,
+      disableClose,
+      data: {
+        controlType: ModalType.mConfirm,
+        title,
+        description,
+        btnCloseTitle,
+        btnConfirmTitle,
+        icon,
+        mBackgroundColorIcon,
+        useMatIcon
+      }
+    });
+    return response.componentInstance.isConfirmed;
+  }
 }
