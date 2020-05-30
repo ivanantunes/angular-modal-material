4e772e4b8bc449d1685f0e9252430b1309c4cca1
Ivan Antunes <ivanantnes75@gmail.com>
2020-05-29 21:09:59 -0300

    Teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { ModalMaterialService } from 'modal-material';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private modal: ModalMaterialService) {
+    modal.mTSuccess('Teste', 'Teste de Descricao', 'Fechar', 'auto', 'auto', true);
+  }
+
   title = 'angular-modal-material';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ModalMaterialModule } from 'modal-material';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ModalMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
