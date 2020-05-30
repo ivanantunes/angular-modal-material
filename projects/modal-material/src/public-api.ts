20e315b2b2c743592718d0bb56d7f7d25b882ba7
Ivan Antunes <ivanantnes75@gmail.com>
2020-05-29 21:01:36 -0300

    export lib

---
+++ projects/modal-material/src/public-api.ts
@@ -0,0 +1,5 @@
+export * from './lib/modal-material.service';
+export * from './lib/modal-material.module';
+export * from './lib/modal-material.component';
+export * from './lib/interfaces/modal';
+export * from './lib/interfaces/modal-type';
